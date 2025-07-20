d55ea0335d84986bc451ca7f21a0d55cae04d085
DLipson <59182090+DLipson@users.noreply.github.com>
2025-07-20 18:11:05 +0300

    Integrate Base Web and Styletron providers in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+// BaseUI and Styletron setup
+import { StyletronProvider } from "baseui";
+import { BaseProvider, LightTheme } from "baseui";
+import { Client } from "styletron-engine-atomic";
+
+const engine = new Client();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <App />
+      </BaseProvider>
+    </StyletronProvider>
+  </StrictMode>
+);
